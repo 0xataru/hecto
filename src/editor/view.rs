3071f402362dee17e6e5d67df5a53808ab3cc68e
0xataru <0xataru@proton.me>
2025-03-02 01:30:54 +0200

    better rendering

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -58,21 +58,6 @@ impl View {
         Terminal::print(line_text)?; 
         Ok(())
     }
-
-    fn draw_welcome_message() -> Result<(), Error> {
-        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
-        let width = Terminal::size()?.width;
-        let len = welcome_message.len();
-        // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
-        // it's allowed to be a bit to the left or right.
-        #[allow(clippy::integer_division)]
-        let padding = (width.saturating_sub(len)) / 2;
-        let spaces = " ".repeat(padding.saturating_sub(1));
-        welcome_message = format!("~{spaces}{welcome_message}");
-        welcome_message.truncate(width);
-        Terminal::print(&welcome_message)?;
-        Ok(())
-    }
     
     fn build_welcome_message(width: usize) -> String {
         if width == 0 {
@@ -90,12 +75,6 @@ impl View {
         let mut full_message = format!("~{}{}", " ".repeat(padding), welcome_message);
         full_message.truncate(width);
         full_message
-    }
-
-    fn draw_empty_row() -> Result<(), Error> {
-        Terminal::print("~")?;
-        Ok(())
-            
     }
 
     pub fn load(&mut self, file_name: &str) {
